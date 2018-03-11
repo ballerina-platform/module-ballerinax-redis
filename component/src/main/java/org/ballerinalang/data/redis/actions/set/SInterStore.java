841b0c3914b2e2f8f1dec3c2dbcbd26fe115e452
Manuri <amaya.uom@gmail.com>
2018-03-11 12:32:20 +0530

    Add set actions

---
+++ component/src/main/java/org/ballerinalang/data/redis/actions/set/SInterStore.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http:www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http:www.apache.orglicensesLICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.data.redis.actions.set;
+
+import org.ballerinalang.bre.Context;
+import org.ballerinalang.connector.api.ConnectorFuture;
+import org.ballerinalang.data.redis.Constants;
+import org.ballerinalang.data.redis.RedisDataSource;
+import org.ballerinalang.data.redis.actions.AbstractRedisAction;
+import org.ballerinalang.model.types.TypeKind;
+import org.ballerinalang.model.values.BConnector;
+import org.ballerinalang.model.values.BInteger;
+import org.ballerinalang.model.values.BStringArray;
+import org.ballerinalang.natives.annotations.Argument;
+import org.ballerinalang.natives.annotations.BallerinaAction;
+import org.ballerinalang.util.exceptions.BallerinaException;
+
+/**
+ * {@code {@link SInterStore}} Maps with "SINTERSTORE" operation of Redis.
+ *
+ * @since 0.5.0
+ */
+@BallerinaAction(packageName = "ballerina.data.redis",
+                 actionName = "sInterStore",
+                 connectorName = Constants.CONNECTOR_NAME,
+                 args = {
+                         @Argument(name = "c",
+                                   type = TypeKind.CONNECTOR)
+                 })
+public class SInterStore extends AbstractRedisAction {
+
+    @Override
+    public ConnectorFuture execute(Context context) {
+        BConnector bConnector = (BConnector) getRefArgument(context, 0);
+        RedisDataSource<String, String> redisDataSource = getDataSource(bConnector);
+
+        String dest = getStringArgument(context, 0);
+        BStringArray keys = (BStringArray) getRefArgument(context, 1);
+        if (keys == null) {
+            throw new BallerinaException("Key array " + MUST_NOT_BE_NULL);
+        }
+        BInteger result = sInterStore(dest, redisDataSource, createArrayFromBStringArray(keys));
+        context.getControlStack().getCurrentFrame().returnValues[0] = result;
+        return getConnectorFuture();
+    }
+}
