6c4ee67b54d7afe9b842ddd555f3d166e5dc03f1
Manuri <amaya.uom@gmail.com>
2018-03-11 12:33:23 +0530

    Add key actions

---
+++ component/src/main/java/org/ballerinalang/data/redis/actions/key/Del.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http:www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http:www.apache.orglicensesLICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.data.redis.actions.key;
+
+import org.ballerinalang.bre.Context;
+import org.ballerinalang.connector.api.ConnectorFuture;
+import org.ballerinalang.data.redis.Constants;
+import org.ballerinalang.data.redis.RedisDataSource;
+import org.ballerinalang.data.redis.actions.AbstractRedisAction;
+import org.ballerinalang.model.types.TypeKind;
+import org.ballerinalang.model.values.BConnector;
+import org.ballerinalang.model.values.BInteger;
+import org.ballerinalang.model.values.BStringArray;
+import org.ballerinalang.natives.annotations.Argument;
+import org.ballerinalang.natives.annotations.BallerinaAction;
+
+/**
+ * {@code {@link Del}} Maps with "DEL" operation of Redis.
+ *
+ * @since 0.5.0
+ */
+@BallerinaAction(packageName = "ballerina.data.redis",
+                 actionName = "del",
+                 connectorName = Constants.CONNECTOR_NAME,
+                 args = {
+                         @Argument(name = "c",
+                                   type = TypeKind.CONNECTOR)
+                 })
+public class Del extends AbstractRedisAction {
+
+    @Override
+    public ConnectorFuture execute(Context context) {
+        BConnector bConnector = (BConnector) getRefArgument(context, 0);
+        RedisDataSource<String, String> redisDataSource = getDataSource(bConnector);
+
+        BStringArray keys = (BStringArray) getRefArgument(context, 1);
+        BInteger result = del(redisDataSource, createArrayFromBStringArray(keys));
+        context.getControlStack().getCurrentFrame().returnValues[0] = result;
+        return getConnectorFuture();
+    }
+}
---
+++ component/src/main/java/org/ballerinalang/data/redis/actions/key/RandomKey.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http:www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http:www.apache.orglicensesLICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.data.redis.actions.key;
+
+import org.ballerinalang.bre.Context;
+import org.ballerinalang.connector.api.ConnectorFuture;
+import org.ballerinalang.data.redis.Constants;
+import org.ballerinalang.data.redis.RedisDataSource;
+import org.ballerinalang.data.redis.actions.AbstractRedisAction;
+import org.ballerinalang.model.types.TypeKind;
+import org.ballerinalang.model.values.BConnector;
+import org.ballerinalang.model.values.BString;
+import org.ballerinalang.natives.annotations.Argument;
+import org.ballerinalang.natives.annotations.BallerinaAction;
+
+/**
+ * {@code {@link RandomKey}} Maps with "RANDOMKEY" operation of Redis.
+ *
+ * @since 0.5.0
+ */
+@BallerinaAction(packageName = "ballerina.data.redis",
+                 actionName = "randomKey",
+                 connectorName = Constants.CONNECTOR_NAME,
+                 args = {
+                         @Argument(name = "c",
+                                   type = TypeKind.CONNECTOR)
+                 })
+public class RandomKey extends AbstractRedisAction {
+
+    @Override
+    public ConnectorFuture execute(Context context) {
+        BConnector bConnector = (BConnector) getRefArgument(context, 0);
+        RedisDataSource<String, String> redisDataSource = getDataSource(bConnector);
+
+        BString result = randomKey(redisDataSource);
+        context.getControlStack().getCurrentFrame().returnValues[0] = result;
+        return getConnectorFuture();
+    }
+}
