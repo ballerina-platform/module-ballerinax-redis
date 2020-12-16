b5c11b6f0b50774a934b50f392b1647d664edc3d
Kapilraaj <72600285+kapilraajP@users.noreply.github.com>
2020-12-16 10:34:22 +0530

    Update RedisDbUtils.java

--- redis-utils/src/main/java/org/ballerinalang/redis/utils/RedisDbUtils.java
+++ redis-utils/src/main/java/org/ballerinalang/redis/utils/RedisDbUtils.java
@@ -76,6 +76,7 @@ public class RedisDbUtils {
             String line;
             while ((line = reader.readLine()) != null) {
                 output.append(line).append("\n");
+                System.out.println(output.append(line).append("\n"));
             }
             int exitVal = process.waitFor();
             if (exitVal == 0) {
